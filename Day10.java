60f364182bf3b4ca93f16a30b3f71b529378e046
Smsahu1 <smsahu9@gmail.com>
2021-01-31 14:49:14 +0000

    30 Days HackerRank Solution

---
+++ Day10.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day10 {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int count=0;
+        int minCount = 0;
+        
+        while(n > 0){
+            int rem = n % 2;
+            if(rem == 1){
+                count++;
+                if(count > minCount){
+                    minCount = count;
+                }
+            }else{
+                count = 0;
+            }
+            n = n/2;
+        }
+        System.out.println(minCount);
+    }
+}
