6360515f712e1592efd2fa9ae61c474ab16d9890
Smsahu1 <smsahu9@gmail.com>
2021-03-28 18:55:42 +0530

    30 Days HackerRank Solution

---
+++ Gfg_practice_Set/Max_Min.java
@@ -0,0 +1,16 @@
+package Gfg_practice_Set;
+
+public class Max_Min {
+    //Maximum and minimum of an array using minimum number of comparisons
+    public static void main(String[] args) {
+        int Min = 0;
+        int Max = 0;
+        int a[] = new int[]{1,2,3,4,5};
+
+        if(a[0] > a[1]){
+            System.out.println(Max);
+        }
+        else if(a[])
+    }
+    
+}
