2272c9585b69e15eff1f94fa13923a1d2ab67359
Smsahu1 <smsahu9@gmail.com>
2021-04-03 09:50:39 +0530

    30 Days HackerRank Solution

---
+++ Day16.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day16{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        try{
+            String S  = in.next();
+            int I = Integer.parseInt(S);
+            System.out.println(I);
+        }catch(Exception e){
+            System.out.println("Bad String");
+        }
+        
+    }
+
+}
